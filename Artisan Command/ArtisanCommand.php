5f8682ad3ee74dd3149b70958e3b3e5b25a00037
sabuzwebdev <frsmgt@gmail.com>
2018-02-26 13:21:20 +0600

    Artisan Command is updated

--- Artisan Command/ArtisanCommand.php
+++ Artisan Command/ArtisanCommand.php
@@ -1,2 +1,3 @@
 php artisan serve        //For starting artisan server. By default it loads on port 8000
-php artisan serve --port=8080    //it will open on port 8080
+php artisan serve --port=8080    //it will open on port 8080 in windows platform
+sudo php artisan serve --port=80 //it will open on port 80 in unix operating system
